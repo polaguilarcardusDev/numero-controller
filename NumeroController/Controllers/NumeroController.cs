87afeb320d62524365f9853e07ef06a98ae348de
Pol Aguilar <polete1234@hotmail.com>
2024-08-28 18:44:56 +0200

    ADD Proyecto

---
+++ NumeroController/Controllers/NumeroController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NumeroController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumeroController : ControllerBase
+    {
+        // Método POST que recibe un número y devuelve el número incrementado en 1
+        [HttpPost]
+        [Route("Incrementar")]
+        public IActionResult Incrementar([FromBody] int numero)
+        {
+            int resultado = numero + 1;
+            return Ok(resultado);
+        }
+    }
+}
